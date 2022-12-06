5718aedbc87a5f0d89f17891a3bf42b87080adb6
shubh <27shubhamkumar@gmail.com>
2022-12-06 13:29:54 +0530

    second code

---
+++ securitygp.tf
@@ -0,0 +1,35 @@
+resource "aws_security_group" "my-sg" {
+  name        = "my-sg"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.vpc.ipv6_cidr_block]
+  }
+
+   ingress {
+    description      = "ssh from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [aws_vpc.vpc.cidr_block]
+    ipv6_cidr_blocks = [aws_vpc.vpc.ipv6_cidr_block]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "my-sg"
+  }
+}
